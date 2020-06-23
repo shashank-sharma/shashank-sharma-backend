e442d4f3103a1c64a7e2569769c74abd1806f8f2
Shashank Sharma <shashank.sharma98@gmail.com>
2020-06-23 20:45:53 +0530

    Added docs app

---
+++ docs/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Document(models.Model):
+    """
+    Originally implemented for Google Sheets
+
+    """
+    name = models.CharField(max_length=40)
+    type = models.CharField(max_length=40)
+    description = models.CharField(max_length=80)
+    url = models.CharField(max_length=400)
+    added_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
